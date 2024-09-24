0a92462df45b761f48781025dfe7ea0fc56889be
Jeffrey Manning <jeffmgpfit@yahoo.com>
2024-09-24 18:39:31 -0400

    phase one and two complete

---
+++ employee.js
@@ -0,0 +1,15 @@
+class Employee {
+    constructor (name, salary, title, manager = null) {
+        this.name = name;
+        this.salary = salary;
+        this.title = title;
+        this.manager = manager;
+
+    }
+}
+
+module.exports = Employee
+
+
+// const leo = new Employee('Leonardo', 90000, 'Ninja', 'John');
+// console.log(leo)

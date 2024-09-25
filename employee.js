55d90d8baaa36408855f26ba1f91b5ebc1fa996a
Jeffrey Manning <jeffmgpfit@yahoo.com>
2024-09-25 12:57:04 -0400

    DONE!

--- employee.js
+++ employee.js
@@ -23,8 +23,8 @@ module.exports = Employee
 // const leo = new Employee('Leonardo', 90000, 'Ninja', 'John');
 // console.log(leo)
 
-const raph = new Employee('Raphael', 90000, 'Ninja');
-const donny = new Employee('Donatello', 85000, 'Grasshopper');
+// const raph = new Employee('Raphael', 90000, 'Ninja');
+// const donny = new Employee('Donatello', 85000, 'Grasshopper');
 
-console.log(raph.calculateBonus(0.25)); // => 22500
-console.log(donny.calculateBonus(0.15)); // => 12750
+// console.log(raph.calculateBonus(0.25)); // => 22500
+// console.log(donny.calculateBonus(0.15)); // => 12750
